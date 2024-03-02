ac4c26aaf728af1a41de5a9d78d0bfc77a4ecd58
Victor Campelo <victor_campelo@outlook.com>
2024-03-02 16:29:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from threading import Thread
+import base64
 
 app = Flask(__name__)
 
