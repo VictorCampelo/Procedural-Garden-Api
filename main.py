01c55f9ae4d3e8ad8d1925f4f6513abd71624b4c
Victor Campelo <victor_campelo@outlook.com>
2024-03-03 12:54:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -81,6 +81,71 @@ def download_image():
     except Exception as e:
         print("Error downloading image:", e)
 
+def download_N_image():
+    global last_image_path
+    try:
+        # Ensure that the download directory exists
+        os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+        
+        # Configure ChromeOptions
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
+        # Set download directory preference
+        prefs = {"download.default_directory": DOWNLOAD_DIR}
+        chrome_options.add_experimental_option("prefs", prefs)
+
+        # Create WebDriver instance
+        driver = webdriver.Chrome(options=chrome_options)
+
+        # Navigate to the website
+        driver.get('https://jardimprocedural.netlify.app')
+
+        # Find and click the select element
+        select_element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, 'label_control_02'))
+        )
+        select_element.click()
+
+        # Wait for the generate button to appear and click it
+        generate_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'btn1'))
+        )
+        generate_button.click()
+
+        time.sleep(300)
+
+        # Wait for the download button to appear and click it
+        download_button = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable((By.ID, 'download'))
+        )
+        download_button.click()
+
+        # Sleep for 5 seconds to ensure all downloads are complete
+        time.sleep(5)
+
+        # Get the list of files in the download directory
+        downloaded_files = os.listdir(DOWNLOAD_DIR)
+
+        # Filter the list to find the image file
+        image_files = [file for file in downloaded_files if file.endswith('.png')]
+
+        # Check if there are image files
+        if image_files:
+            # Sort the image files by name
+            sorted_image_files = sorted(image_files)
+
+            # Get the file name of the downloaded image
+            last_image_path = os.path.join(DOWNLOAD_DIR, sorted_image_files[-1])
+
+        # Close the browser
+        driver.quit()
+    except Exception as e:
+        print("Error downloading image:", e)
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -89,6 +154,12 @@ def hello_world():
 def start_download_image():
     # Start the download process in a separate thread
     Thread(target=download_image).start()
+    return jsonify({'message': 'Image download started.'}), 200
+
+@app.route('/download_image', methods=['GET'])
+def start_download_N_image():
+    # Start the download process in a separate thread
+    Thread(target=download_N_image).start()
     return jsonify({'message': 'Image download started.'}), 200
 
 @app.route('/last_image', methods=['GET'])
